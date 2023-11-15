ff05e0732fbf69581d3fa1b3d5e8580e23005471
thelunartear <108967089+thelunartear@users.noreply.github.com>
2023-11-15 17:30:15 +0900

    Title: 밭의 높이를 고르게하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231115/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 231115/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 using namespace std;
 
 int main() {
     int N, H, T;
     cin >> N >> H >> T;
 
-    vector<int> height(N);
+    vector<int> heights(N);
     for (int i = 0; i < N; ++i) {
-        cin >> height[i];
+        cin >> heights[i];
     }
 
-    vector<vector<int>> dp(N + 1, vector<int>(T + 1, INT_MAX / 2));
-    dp[0][0] = 0;
+    int minCost = N * T; // 최대 비용으로 초기화
 
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j <= T; ++j) {
-            // If the current height is already greater than or equal to H
-            if (height[i] >= H) {
-                dp[i + 1][j] = min(dp[i + 1][j], dp[i][j]);
-            } else {
-                // Incrementing the height by 1
-                dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j] + (H - height[i]));
-                // Decreasing the height by 1 if the current count is greater than 0
-                if (j > 0) {
-                    dp[i + 1][j - 1] = min(dp[i + 1][j - 1], dp[i][j]);
-                }
+    for (int i = 0; i <= N - T; ++i) {
+        int cost = 0;
+        for (int j = i; j < i + T; ++j) {
+            if (heights[j] < H) {
+                cost += H - heights[j];
+            } else if (heights[j] > H) {
+                cost += heights[j] - H;
             }
         }
-    }
-
-    int minCost = INT_MAX;
-    for (int j = 0; j <= T; ++j) {
-        minCost = min(minCost, dp[N][j]);
+        minCost = min(minCost, cost);
     }
 
     cout << minCost << endl;
