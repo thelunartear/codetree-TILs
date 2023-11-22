1b5af4f74295be75c1964d22912ac7d989ff8186
thelunartear <108967089+thelunartear@users.noreply.github.com>
2023-11-23 00:27:20 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231123/한 가지로 열리는 자물쇠/one-way-lock.cpp
+++ 231123/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -9,19 +9,15 @@ int main() {
     cin>>a>>b>>c;
 
     int count=0;
-    for(int i=1; i<=n; i++)
+
+    for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=n;j++)
         {
             for(int k=1;k<=n;k++)
             {
-                 if (abs(i - j) <= 2 || abs(j - k) <= 2 || abs(k - i) <= 2) 
-                {
-                    if (i != j && j != k && k != i) 
-                    {
-                        count++;
-                    }
-                }
+                if(abs(a-i)<=2 || abs(b-j)<=2 || abs(c-k)<=2)
+                    count++;
             }
         }
     }
