d038af1300a355898932b9a773bad851bd7f1ae4
thelunartear <108967089+thelunartear@users.noreply.github.com>
2023-11-24 11:58:10 +0900

    Title: 개발자의 능력 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231124/개발자의 능력 3/ability-of-developer-3.cpp
+++ 231124/개발자의 능력 3/ability-of-developer-3.cpp
@@ -3,37 +3,35 @@
 #include <algorithm>
 using namespace std;
 
-int GetDiff(int i, int j, int arr[]) {
-    int sum1 = arr[i] + arr[j];
-    int sum2 =  0;
-    for(int k = 0; k < 6; k++)
-        sum2 += arr[k];
-    sum2 -= sum1;
-    return abs(sum1 - sum2);
+int arr[6];
+
+int GetDiff(int i, int j, int k) {
+    int sum1=arr[i] + arr[j] + arr[k];
+    int sum2=0;
+    for (int l=0; l<6; l++)
+        sum2+=arr[l];
+    sum2-=sum1;
+    return abs(sum1-sum2);
 }
 
-
 int main() {
-    int arr[6];
 
-    for(int i=0;i<6;i++)
-    {
-        int a;
-        cin>>a;
-        arr[i]=a;
+    for (int i=0; i<6; i++) {
+        cin>>arr[i];
     }
 
-    int min_diff=INT_MAX;
+    int min_diff = INT_MAX;
 
-    for(int i=0;i<6;i++)
-    {
-        for(int j=i+1;j<6;j++)
-        {
-            min_diff=min(min_diff, GetDiff(i,j,arr));
+    for (int i=0; i<6; i++) {
+        for (int j=i+1; j<6; j++) {
+            for(int k=j+1;k<6;k++)
+            {
+                min_diff = min(min_diff, GetDiff(i, j, k));
+            }
         }
     }
 
-    cout<<min_diff;
+    cout << min_diff;
 
     return 0;
 }
