f9e2b501809bd19c800a847b542b8f06f36a9e6d
thelunartear <108967089+thelunartear@users.noreply.github.com>
2023-12-05 16:55:29 +0900

    Title: 개발팀의 능력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231205/개발팀의 능력/the-capabilities-of-the-development-team.cpp
+++ 231205/개발팀의 능력/the-capabilities-of-the-development-team.cpp
@@ -1,64 +1,55 @@
 #include <iostream>
 #include <algorithm>
 #include <climits>
+#include <vector>
 
 using namespace std;
 
 int skills[5];
 
-int Diff(int i, int j, int k, int l)
-{
-    int total_sum=0;
-    for(int l=0;l<5;l++)
-    {
-        total_sum+=skills[l];
+int diff(int i, int j, int k, int l) {
+    int team1 = skills[i] + skills[j];
+    int team2 = skills[k] + skills[l];
+    int team3 = 0;
+
+    for (int idx = 0; idx < 5; ++idx) {
+        if (idx != i && idx != j && idx != k && idx != l) {
+            team3 += skills[idx];
+        }
     }
 
-    int sum1=skills[i]+skills[j];
-    int sum2=skills[k]+skills[l];
-    int sum3=total_sum-sum1-sum2;
-
-    if(sum1==sum2 || sum2==sum3 || sum3==sum1)
-        return -1;
-
-    int diff=abs(sum1-sum2);
-    diff=max(diff, abs(sum2-sum3));
-    diff=max(diff, abs(sum3-sum1));
-
-    return diff;
+    return max({team1, team2, team3}) - min({team1, team2, team3});
 }
 
-
 int main() {
-    for(int i=0;i<5;i++)
-    {
-        cin>>skills[i];
+    for (int i = 0; i < 5; ++i) {
+        cin >> skills[i];
     }
 
-    int min_diff=INT_MAX;
-    bool flag=true;
-    for(int i=0;i<5;i++)
-    {
-        for(int j=i+1;j<5;j++)
-        {
-            for(int k=0;k<5;k++)
-            {
-                for(int l=k+1;l<5;l++)
-                {
-                    if(i==l||i==k||j==l||j==k)
-                        continue;
-                    if(Diff(i,j,k,l) != -1)
-                    {
-                        min_diff=(min_diff, Diff(i,j,k,l));
-                        flag=false;
+    int min_diff = INT_MAX;
+    bool found = false;
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = i + 1; j < 5; ++j) {
+            for (int k = 0; k < 5; ++k) {
+                for (int l = k + 1; l < 5; ++l) {
+                    if (i != l && i != k && j != l && j != k) {
+                        int curr_diff = diff(i, j, k, l);
+                        if (curr_diff != 0 && curr_diff < min_diff) {
+                            min_diff = curr_diff;
+                            found = true;
+                        }
                     }
                 }
             }
         }
     }
 
-    if(flag) cout<<-1;
-    else cout<<min_diff;
+    if (found) {
+        cout << min_diff << endl;
+    } else {
+        cout << -1 << endl;
+    }
 
     return 0;
 }
