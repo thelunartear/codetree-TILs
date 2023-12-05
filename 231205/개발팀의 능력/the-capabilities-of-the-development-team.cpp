32e6006d325394955b0c0ea6e6a8e61232d67330
thelunartear <108967089+thelunartear@users.noreply.github.com>
2023-12-05 16:35:53 +0900

    Title: 개발팀의 능력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231205/개발팀의 능력/the-capabilities-of-the-development-team.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int skills[5];
+
+int Diff(int i, int j, int k, int l)
+{
+    int total_sum=0;
+    for(int l=0;l<5;l++)
+    {
+        total_sum+=skills[l];
+    }
+
+    int sum1=skills[i]+skills[j];
+    int sum2=skills[k]+skills[l];
+    int sum3=total_sum-sum1-sum2;
+
+    if(sum1==sum2 || sum2==sum3 || sum3==sum1)
+        return -1;
+
+    int diff=abs(sum1-sum2);
+    diff=max(diff, abs(sum2-sum3));
+    diff=max(diff, abs(sum3-sum1));
+
+    return diff;
+}
+
+
+int main() {
+    for(int i=0;i<5;i++)
+    {
+        cin>>skills[i];
+    }
+
+    int min_diff=INT_MAX;
+    bool flag=true;
+    for(int i=0;i<5;i++)
+    {
+        for(int j=i+1;j<5;j++)
+        {
+            for(int k=0;k<5;k++)
+            {
+                for(int l=k+1;l<5;l++)
+                {
+                    if(i==l||i==k||j==l||j==k)
+                        continue;
+                    if(Diff(i,j,k,l) != -1)
+                    {
+                        min_diff=(min_diff, Diff(i,j,k,l));
+                        flag=false;
+                    }
+                }
+            }
+        }
+    }
+
+    if(flag) cout<<-1;
+    else cout<<min_diff;
+
+    return 0;
+}
