370aaaa8f856f2bb91aaca6dde655af5844518c1
thelunartear <108967089+thelunartear@users.noreply.github.com>
2023-12-07 12:42:41 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231207/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 231207/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
 
-    vector<pair<int, int>> segments;
+    int x1[n], x2[n];
 
-    for (int i = 0; i < N; ++i) {
-        int x1, x2;
-        cin >> x1 >> x2;
-        segments.push_back({x1, x2});
+    for(int i=0;i<n;i++)
+    {
+        cin>>x1[i]>>x2[i];
     }
 
-    // 선분을 시작점을 기준으로 정렬합니다.
-    sort(segments.begin(), segments.end());
+    int ans=0;
 
-    int count = 1; // 겹치지 않는 첫 번째 선분
-    int right = segments[0].second; // 현재 가장 우측 끝점
+    for(int i=0;i<n;i++)
+    {
+        bool overlap=false;
+        for(int j=0;j<n;j++)
+        {
+            if(j==i) continue;
 
-    for (int i = 1; i < N; ++i) {
-        if (segments[i].first > right) { // 겹치지 않는 선분인 경우
-            ++count;
-            right = segments[i].second; // 새로운 우측 끝점으로 갱신
-        } else {
-            right = max(right, segments[i].second); // 겹치는 경우 우측 끝점 갱신
+            if((x1[i] <= x1[j] && x2[i] >= x2[j]) || (x1[i] >= x1[j] && x2[i] <= x2[j]))
+            {
+                overlap=true;
+                break;
+            }
+
+            if(overlap==false)
+                ans++;
         }
-    }
-    
-    cout << count << endl;
+    }    
+
+    cout<<ans;
 
     return 0;
 }
