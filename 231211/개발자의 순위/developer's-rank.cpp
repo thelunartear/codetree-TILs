bf04c47ea2b5326b7ceca67f9db9c02b0a323c30
thelunartear <108967089+thelunartear@users.noreply.github.com>
2023-12-11 15:55:12 +0900

    Title: 개발자의 순위, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231211/개발자의 순위/developer's-rank.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int k, n;
+    cin>>k>>n;
+
+    int arr[k][n];
+    
+    for(int i=0;i<k;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    int cnt=0;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(i==j) continue;
+            bool flag=true;
+
+            for(int x=0;x<k;x++)
+            {
+                int ii=0, ij=0;
+                for(int y=0;y<n;y++)
+                {
+                    if(arr[x][y]==i)
+                        ii=y;
+                    if(arr[x][y]==j)
+                        ij=y;
+                }
+                if(ii > ij) flag = false;
+            }
+            if(flag) cnt++;
+        }
+    }
+
+    cout<<cnt;
+
+    return 0;
+}
