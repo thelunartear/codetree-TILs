d3ec8d990a0d5086599abae3d7e007ce77f589f1
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-01-02 00:04:39 +0900

    Title: 선택 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n-1;i++)
+    {
+        int minimum=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[minimum])
+            {
+                minimum=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[minimum];
+        arr[minimum]=temp;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<' ';
+    }
+    return 0;
+}
