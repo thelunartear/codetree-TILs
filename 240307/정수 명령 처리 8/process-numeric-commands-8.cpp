76e9ee7add5c0b9b4a7836b09beb6b103a8a3e38
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-03-07 11:59:04 +0900

    Title: 정수 명령 처리 8, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240307/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    list<int> l;
+    for(int i=0;i<n;i++)
+    {
+        string command;
+        cin>>command;
+        if(command=="push_back")
+        {
+            int val;
+            cin>>val;
+            l.push_back(val);
+        }
+        else if(command=="push_front")
+        {
+            int val;
+            cin>>val;
+            l.push_front(val);
+        }
+        else if(command=="pop_front")
+        {
+            cout<<l.front()<<endl;
+            l.pop_front();
+        }
+        else if(command=="pop_back")
+        {
+            cout<<l.back()<<endl;
+            l.pop_back();
+        }
+        else if(command=="size")
+        {
+            cout<<l.size()<<endl;
+        }
+        else if(command=="empty")
+        {
+            if(l.size()==0) cout<<1<<endl;
+            else cout<<0<<endl;
+        }
+        else if(command=="front")
+        {
+            cout<<l.front()<<endl;
+        }
+        else if(command=="back")
+        {
+            cout<<l.back()<<endl;
+        }
+    }
+    return 0;
+}
