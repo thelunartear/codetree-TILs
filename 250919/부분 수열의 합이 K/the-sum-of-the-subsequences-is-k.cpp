1b38f2d1de273f8d75d14c4e339c97156a2307fb
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-09-19 21:31:00 +0900

    Title: 부분 수열의 합이 K, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250919/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.cpp
+++ 250919/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.cpp
@@ -16,14 +16,14 @@ int main() {
     int ans=0;
     for(int i=1;i<=n;i++)
     {
-        psum[i]=psum[i-1]+arr[i];
+        psum[i]=psum[i-1]+arr[i-1];
     }
 
-    for(int i=1;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<n+1;j++)
+        for(int j=i;j<n;j++)
         {
-            if((psum[j]-psum[i]+arr[i])==k)
+            if((psum[j+1]-psum[i])==k)
                 ans++;
         }
     }
